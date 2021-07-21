4afc95e33bf8a227452f2dea5137053ec087ba22
Caroline <Cluce059@gmail.com>
2021-07-21 16:50:59 -0400

    html skeleton

--- Develop/assets/script.js
+++
@@ -1,1 +0,0 @@
-var createTask = function(tast)
---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var dayEl = document.querySelector("#schedule-day-wrapper");
+var hourEl = document.querySelector("#hour-col");
+var taskEl = document.querySelector("#task-item");
+var taskNameEl = document.querySelector("#task-name");
+var saveButtonEl = document.querySelector("#save-btn");
+var tasks = [];
+var now = moment().format("dddd MMM Do");
+$("#currentDay").html(now);
+
+//display current date and time
+
+$(document).ready(function(){
+    //listen for save btn click
+    //save the task-item text in localstorage
+    //
+});
+
