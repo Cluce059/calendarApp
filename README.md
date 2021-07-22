0fe9a8547161016311dcedce3e5a95bd224d211a
Caroline <Cluce059@gmail.com>
2021-07-22 14:26:02 -0400

    fixed saving hour-block to local bug

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Work Day Scheduler Starter Code
+
+## Purpose
+An application to help the user visualize their daily tasks in relation to the current time of day.
+The workday spans from 9am to 5pm, but can be refactored for any time span.
+Time blocks in red represent the current time,
+time blocks in green represent the day ahead. Past hours will not be colored in.
+
+## Website
+https://cluce059.github.io/calendarApp/
+
+## screenshot
+[screenshot] ""
+Refactored with ❤️ by Caroline Luce
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 var today = moment().format("dddd MMM Do");
 $("#currentDay").html(today);
 
@@ -15,6 +14,7 @@ $(document).ready(function(){
         var time = $(this).parent().attr("id");
         //save input in local storage
         localStorage.setItem(time, task);
+        var text = $(this).siblings(".description");
         //console.log(time);
         //console.log(task);
     })
@@ -23,9 +23,9 @@ $(document).ready(function(){
     function time(){
     //current time    
     var rightNow = moment().hour();
-    console.log(rightNow);
+    //console.log(rightNow);
     //loop thru the blocks and compare them w current time
-    //NOTE TO SELF i prefer loops but this .each methods will work the 
+    //prefer loops but this .each methods will work the 
     //same might as well use the methods provided by the api...saves me from making
     //an array of each block anyway
     //select using jquery
@@ -33,7 +33,7 @@ $(document).ready(function(){
         //split methods was intriduced in mod 6...already did that so assuming
         //that i can use it to breake uo this newly made string
         var block = parseInt($(this).attr("id").split("hour")[1]);
-        console.log("block=" + block);
+        //console.log("block=" + block);
         //conditionals to decide what css styling to have based on current time
         if(block < rightNow) {
             $(this).removeClass("future");
@@ -59,11 +59,11 @@ $(document).ready(function(){
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
     $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour1 .description").val(localStorage.getItem("hour13"));
-    $("#hour2 .description").val(localStorage.getItem("hour14"));
-    $("#hour3 .description").val(localStorage.getItem("hour15"));
-    $("#hour4 .description").val(localStorage.getItem("hour16"));
-    $("#hour5 .description").val(localStorage.getItem("hour17"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17.description").val(localStorage.getItem("hour17"));
     //call time function
     time();
 });
